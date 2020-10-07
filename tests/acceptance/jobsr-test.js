1b2327e432abf2fece5e62619f9c31fc938aef12
omkarpai <51873597+omkarpai@users.noreply.github.com>
2020-10-07 14:58:16 +0530

    Added navbar and tests

--- tests/acceptance/jobsr-test.js
+++ tests/acceptance/jobsr-test.js
@@ -8,6 +8,8 @@ module('Acceptance | jobsr', function(hooks) {
   test('visiting /', async function(assert) {
     await visit('/');
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals!');
     assert.dom('.jumbo a.button').hasText('About Us');
     await click('.jumbo a.button');
@@ -19,6 +21,8 @@ module('Acceptance | jobsr', function(hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals');
 
     assert.dom('.jumbo a.button').hasText('Contact Us');
@@ -31,6 +35,8 @@ module('Acceptance | jobsr', function(hooks) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('a.button').hasText('Home');
@@ -38,4 +44,25 @@ module('Acceptance | jobsr', function(hooks) {
 
     assert.equal(currentURL(), '/');
   });
+
+  test('navigating using nav bar', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(),'/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(),'/getting-in-touch')
+  });
+
+
 });
